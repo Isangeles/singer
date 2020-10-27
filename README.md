1a81fa61ebe4dc5c37ce34b608bcfd20a2dbd6ce
Dariusz Sikora <dev@isangeles.pl>
2020-10-27 18:12:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,73 @@
+# Introduction
+Singer is a simple, easy-to-use audio player library written in C on the top of GStreamer. 
+
+Currently in an early development stage.
+# Build
+First you need to install [GStreamer](https://gstreamer.freedesktop.org).
+
+After that you can use [Meson](https://mesonbuild.com) to build the static library:
+```
+$ meson build && cd build && ninja
+```
+# Usage
+Create audio player:
+```
+Player *player = singer_create_player();
+if (player->err != NULL) {
+	printf("Unable to create player: %s\n", player->err);
+}
+```
+Set audio file:
+```
+singer_set_track(player, "/path/to/audio/file.ogg");
+if (player->err != NULL) {
+	printf("Unable to set audio track: %s\n", player->err);
+}
+```
+Play audio:
+```
+singer_play(player);
+if (player->err != NULL) {
+	printf("Unable to play audio: %s\n", player->err);
+}
+```
+Pause audio:
+```
+singer_pause(player);
+if (player->err != NULL) {
+	printf("Unable to pause audio: %s\n", player->err);
+}
+```
+Get current track position in milliseconds:
+```
+gint64 pos = singer_track_pos(player);
+printf("Track position: %ldd\n", pos);
+```
+Get current track lenght in milliseconds:
+```
+gint64 len = singer_track_len(player);
+printf("Track lenght: %ldd\n", len);
+```
+# Contributing
+You are welcome to contribute to project development.
+
+If you looking for things to do, then check TODO file or contact maintainer(dev@isangeles.pl).
+
+When you find something to do, create a new branch for your feature. After you finish, open a pull request to merge your changes with master branch.
+# Contact
+* Isangeles <<dev@isangeles.pl>>
+# License
+Copyright (C) 2020 Dariusz Sikora <<dev@isangeles.pl>>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ singer.c
@@ -0,0 +1,238 @@
+/*
+ * singer.c
+ *
+ * Copyright (C) 2020 Dariusz Sikora <dev@isangeles.pl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+#include "singer.h"
+#include <stdio.h>
+
+static struct _Player PLAYER_NEW = {.pipeline = NULL, .source = NULL, .convert = NULL, 
+									.resample = NULL, .sink = NULL, .err = "Not initialized"};
+/* Handler for the pad-added signal. */
+static void pad_added_handler(GstElement *src, GstPad *pad, Player *player);
+void singer_listen(Player *player);
+void singer_background_listen(void *player);
+
+/* Creates new audio player.
+ * In case of an error, err variable in returned Player struct
+ * is not null. */
+Player *singer_create_player()
+{
+    Player *player = &PLAYER_NEW;
+    /* Create the elements */
+    player->source = gst_element_factory_make("uridecodebin", "source");
+    player->convert = gst_element_factory_make("audioconvert", "convert");
+    player->resample = gst_element_factory_make("audioresample", "resample");
+    player->sink = gst_element_factory_make("autoaudiosink", "sink");
+    /* Create the empty pipeline */
+    player->pipeline = gst_pipeline_new("test-pipeline");
+    if (!player->pipeline || !player->source || !player->convert ||
+            !player->resample || !player->sink) {
+        player->err = "Not all elements could be created.";
+        return player;
+    }
+    /* Build the pipeline. */
+    gst_bin_add_many(GST_BIN(player->pipeline), player->source, player->convert,
+                     player->resample, player->sink, NULL);
+    if (!gst_element_link_many(player->convert, player->resample, player->sink,
+                               NULL)) {
+        gst_object_unref(player->pipeline);
+        player->err = "Elements could not be linked.";
+        return player;
+    }
+    /* Connect to the pad-added signal */
+    g_signal_connect(player->source, "pad-added", G_CALLBACK(pad_added_handler),
+                     player);
+    /* Create background thread for player listener */
+    pthread_t thread;
+    int err = pthread_create(&thread, NULL, singer_background_listen,
+                             (void *)player);
+    if (err) {
+        sprintf(player->err, "Unable to create thread for player listener: %d", err);
+        return player;
+    }
+    player->err = NULL;
+    return player;
+}
+
+/* Closes specified audio player. */
+void singer_close_player(Player *player)
+{
+    gst_element_set_state(player->pipeline, GST_STATE_NULL);
+    gst_object_unref(player->pipeline);
+}
+
+/* Function for player listener background thread */
+void singer_background_listen(void *player)
+{
+    singer_listen(player);
+}
+
+/* Listens to the audio player bus and handles messages and
+ * state changes. */
+void singer_listen(Player *player)
+{
+    GstBus *bus;
+    GstMessage *msg;
+    gboolean terminate = FALSE;
+
+    /* Listen to the bus */
+    bus = gst_element_get_bus(player->pipeline);
+    do {
+        msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                         GST_MESSAGE_STATE_CHANGED |
+                                         GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+        /* Parse message */
+        if (msg != NULL) {
+            GError *err;
+            gchar *debug_info;
+
+            switch (GST_MESSAGE_TYPE(msg)) {
+            case GST_MESSAGE_ERROR:
+                gst_message_parse_error(msg, &err, &debug_info);
+                g_printerr("Error received from element %s: %s\n",
+                           GST_OBJECT_NAME(msg->src), err->message);
+                g_printerr("Debugging information: %s\n",
+                           debug_info ? debug_info : "none");
+                g_clear_error(&err);
+                g_free(debug_info);
+                terminate = TRUE;
+                break;
+            case GST_MESSAGE_EOS:
+                g_print("End-Of-Stream reached.\n");
+                terminate = TRUE;
+                break;
+            case GST_MESSAGE_STATE_CHANGED:
+                /* We are only interested in state-changed messages from the pipeline */
+                if (GST_MESSAGE_SRC(msg) == GST_OBJECT(player->pipeline)) {
+                    GstState old_state, new_state, pending_state;
+                    gst_message_parse_state_changed(msg, &old_state, &new_state,
+                                                    &pending_state);
+                    g_print("Pipeline state changed from %s to %s:\n",
+                            gst_element_state_get_name(old_state),
+                            gst_element_state_get_name(new_state));
+                }
+                break;
+            default:
+                /* We should not reach here */
+                g_printerr("Unexpected message received.\n");
+                break;
+            }
+            gst_message_unref(msg);
+        }
+    } while (!terminate);
+    gst_object_unref(bus);
+    singer_close_player(player);
+    return;
+}
+
+/* Sets audio file from specified path as current track. */
+void singer_set_track(Player *player, const char *path)
+{
+    GstStateChangeReturn ret = gst_element_set_state(player->pipeline, GST_STATE_NULL);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        player->err = "Unable to set the pipeline to null state";
+        return;
+    }
+    char uri[7 + strlen(path)];
+    sprintf(uri, "file://%s", path);
+    g_object_set(player->source, "uri", uri, NULL);
+}
+
+/* Starts playing audio file from specified path */
+void singer_play(Player *player)
+{
+    GstStateChangeReturn ret = gst_element_set_state(player->pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        player->err = "Unable to set the pipeline to the playing state";
+    }
+}
+
+/* Pauses audio player */
+void singer_pause(Player *player)
+{
+    GstStateChangeReturn ret = gst_element_set_state(player->pipeline, GST_STATE_PAUSED);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        player->err = "Unable to set the pipeline to the paused state";
+    }
+}
+
+/* Returns player position in milliseconds for current track. */
+gint64 singer_track_pos(Player *player)
+{
+    gint64 pos;
+    if (!gst_element_query_position(player->pipeline, GST_FORMAT_TIME, &pos))
+        pos = 0;
+    return pos;
+}
+
+/* Returns lenght in milliseconds for current track. */
+gint64 singer_track_len(Player *player)
+{
+    gint64 len;
+    if (!gst_element_query_duration(player->pipeline, GST_FORMAT_TIME, &len))
+        len = 0;
+    return len;
+}
+
+/* This function will be called by the pad-added signal. */
+static void pad_added_handler(GstElement *src, GstPad *new_pad,
+                              Player *player)
+{
+    GstPad *sink_pad = gst_element_get_static_pad(player->convert, "sink");
+    GstPadLinkReturn ret;
+    GstCaps *new_pad_caps = NULL;
+    GstStructure *new_pad_struct = NULL;
+    const gchar *new_pad_type = NULL;
+
+    g_print("Received new pad '%s' from '%s':\n", GST_PAD_NAME(new_pad),
+            GST_ELEMENT_NAME(src));
+
+    /* If our converter is already linked, we have nothing to do here */
+    if (gst_pad_is_linked(sink_pad)) {
+        g_print("We are already linked. Ignoring.\n");
+        goto exit;
+    }
+
+    /* Check the new pad's type */
+    new_pad_caps = gst_pad_get_current_caps(new_pad);
+    new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
+    new_pad_type = gst_structure_get_name(new_pad_struct);
+    if (!g_str_has_prefix(new_pad_type, "audio/x-raw")) {
+        g_print("It has type '%s' which is not raw audio. Ignoring.\n",
+                new_pad_type);
+        goto exit;
+    }
+
+    /* Attempt the link */
+    ret = gst_pad_link(new_pad, sink_pad);
+    if (GST_PAD_LINK_FAILED(ret)) {
+        g_print("Type is '%s' but link failed.\n", new_pad_type);
+    } else {
+        g_print("Link succeeded (type '%s').\n", new_pad_type);
+    }
+
+exit:
+    /* Unreference the new pad's caps, if we got them */
+    if (new_pad_caps != NULL)
+        gst_caps_unref(new_pad_caps);
+
+    /* Unreference the sink pad */
+    gst_object_unref(sink_pad);
+}
